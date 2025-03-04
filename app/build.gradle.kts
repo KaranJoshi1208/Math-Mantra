adea9dbd7d740ba2caa83df44ee28841132b3f9e
Karan Joshi <karanjoshi1208@gmail.com>
2025-03-04 06:25:20 +0530

    Dashboard - UI fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.airbnb.android:lottie:4.2.2")
+    implementation("com.github.LottieFiles:dotlottie-android:0.6.2")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.fragment:fragment:1.8.6")
     implementation("androidx.activity:activity:1.7.0")
